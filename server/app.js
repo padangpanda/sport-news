f060b1c73e35fcace35d128b394b8607c27db62e
padangpanda <ppanda.naran@outlook.com>
2021-01-07 21:13:09 +0700

    newsapi

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = require('./routes')
 const app = express()
 const port = 3080
+const ApiController = require('./controllers/ApiController')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(router)
+app.get('/', ApiController.getNewsApiSport)
 
 app.listen(port, () => console.log('listen on port,', port))
