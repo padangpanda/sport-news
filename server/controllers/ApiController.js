5dcd3ca0a7f961e2f5528f51d34d01233d99887f
muhammadhaikal <muh.heykall@gmail.com>
2021-01-08 10:57:44 +0700

    refactor API controller from promise to async

--- server/controllers/ApiController.js
+++ server/controllers/ApiController.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
 
 class ApiController {
-    static getNewsApiSport(req, res, next) {
-        console.log(req.body)
-        let { countryCode } = req.body
+    static async getNewsApiSport(req, res, next) {
+        try {
+            let { countryCode } = req.body
         let timeUrl = ''
         let newsUrl = `https://newsapi.org/v2/top-headlines?country=${countryCode}&category=sports&pageSize=100&apiKey=6792ee54b2db4720b9fe0fee7b7440cd`
-        let obj = {}
-
+        
         switch (countryCode) {
             case 'id':
                 timeUrl = 'http://worldtimeapi.org/api/timezone/Asia/Jakarta'
@@ -21,7 +20,7 @@ class ApiController {
             case 'it':
                 timeUrl = 'http://worldtimeapi.org/api/timezone/Europe/Rome'
                 break;
-            case '':
+            case 'jp':
                 timeUrl = 'http://worldtimeapi.org/api/timezone/Asia/Tokyo'
                 break;
             default:
@@ -30,23 +29,18 @@ class ApiController {
         }
 
         let weatherApi = 'http://api.weatherstack.com/current?access_key=8aec2b3b55f3c908f515aa9b3c859a56&query=fetch:ip'
-        let promiseNews = axios.get(newsUrl)
-        let promiseTime = axios.get(timeUrl)
-        let promiseWeather = axios.get(weatherApi)
-
-        Promise.all([promiseNews, promiseTime, promiseWeather])
-            .then(response => {
-                let contentNews = response[0].data.articles
-                let contentTime = response[1].data
-                let contentWeather = response[2].data
-                const apiDatas = {
-                    contentNews,
-                    contentTime,
-                    contentWeather
-                }
-                res.status(200).json(apiDatas)
-            })
-            .catch(next)
+        let contentNews = await axios.get(newsUrl)
+        let contentTime = await axios.get(timeUrl)
+        let contentWeather = await axios.get(weatherApi)
+        const apiDatas = {
+            contentTime: contentTime.data,
+            contentWeather: contentWeather.data,
+            contentNews: contentNews.data.articles
+        }
+        return res.status(200).json(apiDatas)
+        } catch (err) {
+            next(err)
+        }
     }
 }
 
