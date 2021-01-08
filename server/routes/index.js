fd28ebf94ac00f30075977b6c6f1949ceab7eac3
gianRVN <gianmohar@gmail.com>
2021-01-08 12:55:15 +0700

    Merging-client-ex-2API

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const authRouter = require('./auth')
 const newsRouter = require('./articles')
 const { authenticate } = require('../middlewares/checkToken')
 
-// router.use(authRouter)
+router.use(authRouter)
 
 // router.use(authenticate)
 router.use('/news', newsRouter)
