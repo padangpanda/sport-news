6fb187aa2769b297d9ac47b8c24660efcbde91ad
padangpanda <ppanda.naran@outlook.com>
2021-01-08 08:07:26 +0700

    +time API

--- server/controllers/ApiController.js
+++ server/controllers/ApiController.js
@@ -4,17 +4,43 @@ class ApiController {
     static getNewsApiSport(req, res, next) {
         console.log(req.body)
         let { countryCode } = req.body
-        let url = `https://newsapi.org/v2/top-headlines?country=${countryCode}&category=sports&pageSize=100&apiKey=6792ee54b2db4720b9fe0fee7b7440cd`
+        let timeUrl = ''
+        let newsUrl = `https://newsapi.org/v2/top-headlines?country=${countryCode}&category=sports&pageSize=100&apiKey=6792ee54b2db4720b9fe0fee7b7440cd`
+        let obj = {}
 
-        axios.get(url)
+
+        switch (countryCode) {
+            case 'id':
+                timeUrl = 'http://worldtimeapi.org/api/timezone/Asia/Jakarta'
+                break;
+            case 'us':
+                timeUrl = 'http://worldtimeapi.org/api/timezone/America/New_York'
+                break;
+            case 'de':
+                timeUrl = 'http://worldtimeapi.org/api/timezone/Europe/Berlin'
+                break;
+            case 'it':
+                timeUrl = 'http://worldtimeapi.org/api/timezone/Europe/Rome'
+                break;
+            case '':
+                timeUrl = 'http://worldtimeapi.org/api/timezone/Asia/Tokyo'
+                break;
+            default:
+                timeUrl = 'http://worldtimeapi.org/api/timezone/Asia/Jakarta'
+                break;
+        }
+
+        axios.get(newsUrl)
         .then(response => {
-            res.status(200).json(response.data)
+            // res.status(200).json(response.data)
+            obj.articles = response.data.articles
+            return axios.get(timeUrl)
+        })
+        .then(time => {
+            obj.time = time.data
+            res.status(200).json(obj)
         })
         .catch(next)
-    }
-
-    static getScoreNBAData(req, res, next) {
-
     }
 }
 
